16257e1b9902a5ae1b6bb86f170c384efa0bc88e
luohj <myself35335@163.com>
2017-12-02 15:54:45 +0800

    分享

--- jy-admin-client/src/main/java/org/loxf/jyadmin/client/service/ShareService.java
+++ jy-admin-client/src/main/java/org/loxf/jyadmin/client/service/ShareService.java
@@ -1,4 +1,7 @@
 package org.loxf.jyadmin.client.service;
 
+import org.loxf.jyadmin.base.bean.BaseResult;
+
 public interface ShareService {
+    BaseResult shareInfo(String custId, String detailName, String shareObj, String type);
 }
--- jy-admin-dal/src/main/java/org/loxf/jyadmin/dal/dao/ShareMapper.java
+++ jy-admin-dal/src/main/java/org/loxf/jyadmin/dal/dao/ShareMapper.java
@@ -1,17 +1,11 @@
 package org.loxf.jyadmin.dal.dao;
 
+import org.apache.ibatis.annotations.Param;
 import org.loxf.jyadmin.dal.po.Share;
 
 public interface ShareMapper {
-    int deleteByPrimaryKey(Long id);
+    int existsByObjAndType(@Param("custId") String custId, @Param("type") String type, @Param("shareObj") String shareObj);
 
     int insert(Share record);
 
-    int insertSelective(Share record);
-
-    Share selectByPrimaryKey(Long id);
-
-    int updateByPrimaryKeySelective(Share record);
-
-    int updateByPrimaryKey(Share record);
 }
