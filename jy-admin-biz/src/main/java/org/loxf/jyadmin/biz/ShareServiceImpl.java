9a2268eabddcc0e6a7401d69f175cda022d0de07
luohj <myself35335@163.com>
2017-12-27 18:10:22 +0800

    事务控制

--- jy-admin-biz/src/main/java/org/loxf/jyadmin/biz/ShareServiceImpl.java
+++ jy-admin-biz/src/main/java/org/loxf/jyadmin/biz/ShareServiceImpl.java
@@ -3,6 +3,7 @@ package org.loxf.jyadmin.biz;
 import org.loxf.jyadmin.base.bean.BaseResult;
 import org.loxf.jyadmin.base.constant.BaseConstant;
 import org.loxf.jyadmin.base.util.ImageUtil;
+import org.loxf.jyadmin.base.util.JedisUtil;
 import org.loxf.jyadmin.base.util.MatrixToImageWriter;
 import org.loxf.jyadmin.biz.util.ConfigUtil;
 import org.loxf.jyadmin.client.dto.ShareDto;
@@ -32,6 +33,8 @@ public class ShareServiceImpl implements ShareService {
     private AccountService accountService;
     @Value("#{configProperties['IMAGE.SERVER.PATH']}")
     private String IMG_SERVER_PATH;
+    @Autowired
+    private JedisUtil jedisUtil;
 
     @Override
     public BaseResult<String> createQR(String nickName, String custId){
@@ -74,34 +77,42 @@ public class ShareServiceImpl implements ShareService {
     @Transactional
     public BaseResult shareInfo(String custId, String detailName, String shareObj, String type) {
         boolean addBp = false;
-        if(shareMapper.existsByObjAndType(custId, type, shareObj)==0){
-            addBp = true;
-        }
-        if(addBp){
-            String bp;
-            if(type.equals("VIDEO")) {
-                bp = ConfigUtil.getConfig(BaseConstant.CONFIG_TYPE_BP, "BP_SHARE_VIDEO", "10").getConfigValue();
-            } else if(type.equals("ACTIVE")){
-                bp = ConfigUtil.getConfig(BaseConstant.CONFIG_TYPE_BP, "BP_SHARE_ACTIVE", "10").getConfigValue();
-            } else if(type.equals("RECOMMEND")){
-                bp = ConfigUtil.getConfig(BaseConstant.CONFIG_TYPE_BP, "BP_SHARE_REGISTER", "30").getConfigValue();
-            } else if(type.equals("PAGE")){
-                bp = ConfigUtil.getConfig(BaseConstant.CONFIG_TYPE_BP, "BP_SHARE_OTHER", "5").getConfigValue();
-            } else {
-                bp = "1";
+        String key = "SHARE_INFO_" + custId;
+        if(jedisUtil.setnx(key, "true", 60)>0) {
+            try {
+                if (shareMapper.existsByObjAndType(custId, type, shareObj) == 0) {
+                    addBp = true;
+                }
+                if (addBp) {
+                    String bp;
+                    if (type.equals("VIDEO")) {
+                        bp = ConfigUtil.getConfig(BaseConstant.CONFIG_TYPE_BP, "BP_SHARE_VIDEO", "10").getConfigValue();
+                    } else if (type.equals("ACTIVE")) {
+                        bp = ConfigUtil.getConfig(BaseConstant.CONFIG_TYPE_BP, "BP_SHARE_ACTIVE", "10").getConfigValue();
+                    } else if (type.equals("RECOMMEND")) {
+                        bp = ConfigUtil.getConfig(BaseConstant.CONFIG_TYPE_BP, "BP_SHARE_REGISTER", "30").getConfigValue();
+                    } else if (type.equals("PAGE")) {
+                        bp = ConfigUtil.getConfig(BaseConstant.CONFIG_TYPE_BP, "BP_SHARE_OTHER", "5").getConfigValue();
+                    } else {
+                        bp = "1";
+                    }
+                    // 新增积分
+                    BaseResult baseResult = accountService.increase(custId, null, new BigDecimal(bp), null, "分享" + detailName, null);
+                    if (baseResult.getCode() == BaseConstant.FAILED) {
+                        return baseResult;
+                    }
+                }
+                // 新增分享记录
+                Share share = new Share();
+                share.setCustId(custId);
+                share.setShareObj(shareObj);
+                share.setType(type);
+                shareMapper.insert(share);
+                return new BaseResult();
+            } finally {
+                jedisUtil.del(key);
             }
-            // 新增积分
-            BaseResult baseResult = accountService.increase(custId, null, new BigDecimal(bp), null, "分享" + detailName, null);
-            if(baseResult.getCode()==BaseConstant.FAILED){
-                return baseResult;
-            }
         }
-        // 新增分享记录
-        Share share = new Share();
-        share.setCustId(custId);
-        share.setShareObj(shareObj);
-        share.setType(type);
-        shareMapper.insert(share);
-        return new BaseResult();
+        return new BaseResult(BaseConstant.SUCCESS, "正在处理");
     }
 }
