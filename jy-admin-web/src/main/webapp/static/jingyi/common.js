2b083215954aeca003c732589ffddfbcf59c367f
hongjia.lhj <hongjia.lhj@alibaba-inc.com>
2018-05-22 17:43:17 +0800

    教师头衔多个

--- jy-admin-web/src/main/webapp/static/jingyi/common.js
+++ jy-admin-web/src/main/webapp/static/jingyi/common.js
@@ -68,3 +68,7 @@ String.prototype.endWith=function(str){
     var reg=new RegExp(str+"$");
     return reg.test(this);
 }
+
+String.prototype.replaceAll = function(s1,s2){
+    return this.replace(new RegExp(s1,"gm"),s2);
+}
